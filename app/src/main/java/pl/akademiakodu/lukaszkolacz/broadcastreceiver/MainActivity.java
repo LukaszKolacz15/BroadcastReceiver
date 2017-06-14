4ac5d256055a5ca82cfc3c4b11f3d28e0b4bffed
Lukasz Kolacz <lukaszkolacz15@gmail.com>
2017-06-14 17:44:23 +0200

    some changes

--- app/src/main/java/pl/akademiakodu/lukaszkolacz/broadcastreceiver/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/lukaszkolacz/broadcastreceiver/MainActivity.java
@@ -1,13 +1,55 @@
 package pl.akademiakodu.lukaszkolacz.broadcastreceiver;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int questionId = 5;
+
+    private IntentFilter filter = new IntentFilter("android.provider.telephony.SMS_RECEIVED");
+    private BroadcastReceiver broadcast = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "Masz nowego SMS", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS},questionId);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == questionId){
+            Log.i("Permission", "Mamy pozwolenie");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(broadcast, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcast);
     }
 }
